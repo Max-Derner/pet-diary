7bb541276cac321a0f67e9865fbb8756a9722ac6
Max-Derner <max.derner@gmail.com>
2023-10-30 18:27:30 +0000

    dumb mistake

--- app/support/data_access_layer/get_records.py
+++ app/support/data_access_layer/get_records.py
@@ -6,6 +6,6 @@ from app.support.data_access_layer.helpers import get_pet_table_resource
 def get_all_records_for_pet(name: str) -> List[Dict]:
     pet_table = get_pet_table_resource()
     response = pet_table.query(
-        KeyConditionExpression=Key('name').eq('me')
+        KeyConditionExpression=Key('name').eq(name)
     )
     return response['Items']
